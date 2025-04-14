ecc68a2bdf65c59c599c7c349d89dae3bdc968aa
Ted Lehr <t_l81@txstate.edu>
2025-04-14 11:07:55 -0500

    Improved Comments and variable naming
    - Updated comments for clarity and consistency.
    - Renamed variables to better reflect their purpose.
    - Enhanced documentation in method headers for better understanding.

--- ArrayComponent.java
+++ ArrayComponent.java
@@ -3,39 +3,43 @@ import java.awt.geom.*;
 import javax.swing.*;
 
 /**
-   This component draws an array and marks two elements in the
-   array.
-*/
+ * This component draws an array and sortElements two elements in the
+ * array.
+ */
 public class ArrayComponent extends JComponent {
-   // values is an array of doubles each of which represents the 
+   // values is an array of doubles each of which represents the
    // length of a bar that will be drawn on the display panel;
    private Double[] values;
-   // marked1 and marked2 represent the array bars that will 
+   // sortElement1 and sortElement2 represent the array bars that will
    // filled rather than just draw in outline.
-   private Double marked1;
-   private Double marked2;
+   private Double sortElement1;
+   private Double sortElement2;
 
-   // The 'synchornized' keyword ensures that only one thread can execute this method at a time. 
-   // This prevents multiple threads from simultaneously attempting to paint the panel, 
-   // which could lead to race conditions yielding wierd graphics being drawn. 
-   // We only have one thread doing the drawing so, in this application, the concern is moot. 
+   // The 'synchornized' keyword ensures that only one thread can execute this
+   // method at a time.
+   // This prevents multiple threads from simultaneously attempting to paint the
+   // panel,
+   // which could lead to race conditions yielding wierd graphics being drawn.
+   // We only have one thread doing the drawing so, in this application, the
+   // concern is moot.
    // Calling repaint () in setValues() invokes paintComponent()
    @Override
-   public synchronized void paintComponent(Graphics g)
-   {
-      if (values == null) return;
+   public synchronized void paintComponent(Graphics g) {
+      if (values == null)
+         return;
       // Create a graphics context for drawing. And then determine the width of the
       // bars that represent the lengths of the values in values[].
       // The code also decides whether to fill a rectangle (meaning it's being sorted)
       // or just draw it.
       Graphics2D g2 = (Graphics2D) g;
       int width = getWidth() / values.length;
-      for (int i = 0; i < values.length; i++)
-      {
-         Double v =  values[i];
-         Rectangle2D bar = new Rectangle2D.Double(
-            width * i, 0, width, v);
-         if (v == marked1 || v == marked2)
+      for (int i = 0; i < values.length; i++) {
+         Double valueToBeDrawn = values[i];
+         Rectangle2D bar = new Rectangle2D.Double(width * i, 0, width, valueToBeDrawn);
+         // If either of the current bars being drawn is one
+         // of the elements currently being sorted, fill the bar with
+         // color rather than just draw its outlines.
+         if (valueToBeDrawn == sortElement1 || valueToBeDrawn == sortElement2)
             g2.fill(bar);
          else
             g2.draw(bar);
@@ -43,23 +47,21 @@ public class ArrayComponent extends JComponent {
    }
 
    /**
-      Sets the values to be painted.
-      @param values the array of values to display
-      @param marked1 the first marked element
-      @param marked2 the second marked element
-   */
+    * Sets the values to be painted.
+    * 
+    * @param values       the array of values to display
+    * @param sortElement1 the first element of the pair currenlty being sorted
+    * @param sortElement2 the second element of the pair currenlty being sorted
+    */
    public synchronized void setValues(Double[] values,
-      Double marked1, Double marked2)
-   {
+         Double sortElement1, Double sortElement2) {
       // Create a 'shallow' copy of the original values array.
-      // The copy contains references to the same Double objects 
-      // as in the original array. 
+      // The copy contains references to the same Double objects
+      // as in the original array.
       this.values = (Double[]) values.clone();
-      this.marked1 = marked1;
-      this.marked2 = marked2;
-      // repaint() is an inherited method from JComponent.  Although it 
-      // can take several arguments (see: https://docs.oracle.com/javase/8/docs/api/javax/swing/JComponent.html#repaint-long-int-int-int-int-)
-      // passing no arguments repaints the entire component.
+      this.sortElement1 = sortElement1;
+      this.sortElement2 = sortElement2;
+      // Repaint() invokes the paintComponent() method defined above.
       repaint();
    }
 
